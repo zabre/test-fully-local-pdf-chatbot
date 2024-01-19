e5eb71be09ebfde7183ef4e74b43802a77bbe738
Abderrazak <34687872+zabre@users.noreply.github.com>
2024-01-19 14:58:30 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { ChatWindow } from "@/components/ChatWindow";
 export default function Home() {
   return (
     <ChatWindow
-      emoji="🏠"
-      titleText="Fully Client-Side Chat Over Documents"
-      placeholder="Try asking something about the document you just uploaded!"
+      emoji="🏴‍☠️"
+      titleText="Un agent interne qui lit vos documents à votre place et répond à vos questions"
+      placeholder="La question doit concerner une thématique du corpus de recherche"
     ></ChatWindow>
   );
 }
